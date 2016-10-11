ad633d62cbd9155ee3431e5e255968338d4e6ff5
matts6206 <matts6206@FHC-IC0034099.wrdsb.ca>
2016-10-11 13:57:31 -0400

    Updated some code, added Orthographic camera to fix y coordinates.

--- core/src/gdxtwo/gdxtwo.java
+++ core/src/gdxtwo/gdxtwo.java
@@ -2,43 +2,78 @@ package gdxtwo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.Input.*;
 import com.badlogic.gdx.math.*;
 import java.io.*;
 import com.badlogic.gdx.utils.*;
-//import java.lang.Enum<Cursor.SystemCursor>;
 
 public class gdxtwo extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
         Sprite spCharacter;
         
-        //Vector2 vPos;
         int nPosX, nPosY, CROSSHAIR_CURSOR;
 	
 	@Override
 	public void create () {
+                OrthographicCamera camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+                camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
                 spCharacter = new Sprite(img);
                 spCharacter.setScale(0.5f);
-                //Gdx.graphics.setSystemCursor(Cursor(CROSSHAIR_CURSOR));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.128f, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
                 nPosX = Gdx.input.getX();
                 nPosY = Gdx.graphics.getHeight()-Gdx.input.getY();//Fix Y coordinate to be a better way of getting mouse/tap Y.
 		batch.begin();
 		batch.draw(spCharacter, nPosX-spCharacter.getWidth()/2, nPosY-spCharacter.getHeight()/2);
 		batch.end();
-                //vPos.add(Gdx.input.getX(), Gdx.input.getY());
 	}
 }
+/*
+ * private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            cam.zoom += 0.02;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            cam.zoom -= 0.02;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            cam.translate(-3, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            cam.translate(3, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            cam.translate(0, -3, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            cam.translate(0, 3, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            cam.rotate(-rotationSpeed, 0, 0, 1);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+            cam.rotate(rotationSpeed, 0, 0, 1);
+        }
+
+        cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
+
+        float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+
+        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+    }
+ */
