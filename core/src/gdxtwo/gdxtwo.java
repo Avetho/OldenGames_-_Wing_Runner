975a4062762469929a89fd0f8811698432b109db
matts6206 <matts6206@FHC-IC0034099.wrdsb.ca>
2016-10-12 14:32:17 -0400

    Added input sensors, screen size, framework for escape key to exit and key for full screen, added viewport update for screen size change.

--- core/src/gdxtwo/gdxtwo.java
+++ core/src/gdxtwo/gdxtwo.java
@@ -12,34 +12,99 @@ import com.badlogic.gdx.Input.*;
 import com.badlogic.gdx.math.*;
 import java.io.*;
 import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.utils.viewport.*;
 
-public class gdxtwo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-        Sprite spCharacter;
-        
-        int nPosX, nPosY, CROSSHAIR_CURSOR;
-	
-	@Override
-	public void create () {
-                OrthographicCamera camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-                camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-                spCharacter = new Sprite(img);
-                spCharacter.setScale(0.5f);
-	}
+public class gdxtwo extends ApplicationAdapter implements InputProcessor {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.128f, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-                nPosX = Gdx.input.getX();
-                nPosY = Gdx.graphics.getHeight()-Gdx.input.getY();//Fix Y coordinate to be a better way of getting mouse/tap Y.
-		batch.begin();
-		batch.draw(spCharacter, nPosX-spCharacter.getWidth()/2, nPosY-spCharacter.getHeight()/2);
-		batch.end();
-	}
+    SpriteBatch batch;
+    Texture img;
+    Sprite spCharacter;
+    int nPosX, nPosY, nCursorX, nCursorY;
+    boolean isTouch;
+    
+    private ExtendViewport viewport;
+
+    @Override
+    public void create() {
+        OrthographicCamera camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        if (Gdx.app.getType() == ApplicationType.Android) {
+            int ANDROID_WIDTH = Gdx.graphics.getWidth();
+            int ANDROID_HEIGHT = Gdx.graphics.getHeight();
+            camera = new OrthographicCamera(ANDROID_WIDTH, ANDROID_HEIGHT);
+            camera.translate(ANDROID_WIDTH / 2, ANDROID_HEIGHT / 2);
+            camera.update();
+        } else {
+            camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            camera.translate(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+            camera.update();
+        }
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        spCharacter = new Sprite(img);
+        spCharacter.setScale(0.5f);
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.128f, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        nPosX = Gdx.input.getX();
+        nPosY = Gdx.graphics.getHeight() - Gdx.input.getY();//Fix Y coordinate to be a better way of getting mouse/tap Y.
+        batch.begin();
+        batch.draw(spCharacter, nPosX - spCharacter.getWidth() / 2, nPosY - spCharacter.getHeight() / 2);
+        batch.end();
+    }
+    @Override public void resize (int width, int height) {
+        // viewport must be updated for it to work properly
+        viewport.update(width, height, true);
+    }
+
+
+    @Override
+    public boolean keyDown(int i) {
+        //if(Input.Button == 'Esc')return true;//This senses Escape key to exit program.
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int i) {
+        return false;
+    }
 }
 /*
  * private void handleInput() {
--- desktop/src/gdxtwo/desktop/DesktopLauncher.java
+++ desktop/src/gdxtwo/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import gdxtwo.gdxtwo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+                config.width=1024;
+                config.height=768;
+                config.fullscreen=false;
+                config.foregroundFPS=60;
+                config.backgroundFPS=10;
 		new LwjglApplication(new gdxtwo(), config);
 	}
 }
