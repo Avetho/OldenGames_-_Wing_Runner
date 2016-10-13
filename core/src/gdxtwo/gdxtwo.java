87a4b9273de460f2ef82e2ac6a51c45fb0c378c3
matts6206 <matts6206@FHC-IC0034099.wrdsb.ca>
2016-10-13 13:12:01 -0400

    Added basic sprites and sprite display code. Framework for sprite rotation added, needs char x and y to be float not int.

--- core/src/gdxtwo/gdxtwo.java
+++ core/src/gdxtwo/gdxtwo.java
@@ -14,17 +14,17 @@ import java.io.*;
 import com.badlogic.gdx.utils.*;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.utils.viewport.*;
 
 public class gdxtwo extends ApplicationAdapter implements InputProcessor {
 
     SpriteBatch batch;
-    Texture img;
-    Sprite spCharacter;
+    Texture imgr, imgsp, imgob;
+    Sprite spReticle, spChar, spObs1, spObs2, spObs3;
     int nPosX, nPosY, nCursorX, nCursorY;
     boolean isTouch;
-    
     private ExtendViewport viewport;
 
     @Override
@@ -43,32 +43,57 @@ public class gdxtwo extends ApplicationAdapter implements InputProcessor {
             camera.update();
         }
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        spCharacter = new Sprite(img);
-        spCharacter.setScale(0.5f);
+        imgr = new Texture("badlogic.jpg");
+        imgsp = new Texture("characterSprite0.png");
+        imgob = new Texture("rockObstacle.png");
+        spReticle = new Sprite(imgr);
+        spReticle.setSize(5, 5);
+        spReticle.setCenter(spReticle.getWidth()/2, spReticle.getHeight()/2);
+        spChar = new Sprite(imgsp);
+        spChar.setScale(1, 1);
+        //spChar.setCenter(spChar.getWidth()/2, spChar.getHeight()/2);
+        spChar.setOrigin(spChar.getWidth()/2, spChar.getHeight()/2);
         Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render() {
+        if(Gdx.input.isKeyPressed(Keys.ESCAPE))System.exit(3);
+        //if(Gdx.input.isKeyPressed(Keys.F11))Gdx.graphics.setDisplayMode(Gdx.graphics.);
         Gdx.gl.glClearColor(0.128f, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        nPosX = Gdx.input.getX();
+        nPosX = Gdx.graphics.getWidth()/5;
         nPosY = Gdx.graphics.getHeight() - Gdx.input.getY();//Fix Y coordinate to be a better way of getting mouse/tap Y.
         batch.begin();
-        batch.draw(spCharacter, nPosX - spCharacter.getWidth() / 2, nPosY - spCharacter.getHeight() / 2);
+        batch.draw(imgob, Gdx.graphics.getWidth()-Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/2);
+        batch.draw(imgob, Gdx.graphics.getWidth()-Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()/2-Gdx.graphics.getHeight()/3);
+        batch.draw(imgob, Gdx.graphics.getWidth()-Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/3);
+        batch.draw(imgsp, nPosX, nPosY, spChar.getOriginX(), spChar.getOriginY(), spChar.getHeight(), spChar.getWidth(), spChar.getScaleX(), spChar.getScaleY(), spChar.getRotation());
+        batch.draw(spReticle, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY(), spReticle.getOriginX(), spReticle.getOriginY(), spReticle.getWidth(), spReticle.getHeight(), spReticle.getScaleX(), spReticle.getScaleY(), spReticle.getRotation());
         batch.end();
     }
-    @Override public void resize (int width, int height) {
-        // viewport must be updated for it to work properly
-        viewport.update(width, height, true);
-    }
 
+    @Override
+    public void resize(int width, int height) {
+        // viewport must be updated for it to work properly
+        //viewport.update(width, height, true);
+    }
 
     @Override
     public boolean keyDown(int i) {
-        //if(Input.Button == 'Esc')return true;//This senses Escape key to exit program.
-        return false;
+        /*if (Input == Keys.ESCAPE) {
+            return true;//This senses Escape key to exit program.
+        }
+        if (Input.Buttons == Keys.F11) {
+            if (config.fullscreen == false) {
+                Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayModes());
+            } else {
+                config.fullscreen = false;
+            }
+            return true;
+        } else {*/
+            return false;
+        //}
     }
 
     @Override
@@ -108,37 +133,45 @@ public class gdxtwo extends ApplicationAdapter implements InputProcessor {
 }
 /*
  * private void handleInput() {
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            cam.zoom += 0.02;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
-            cam.zoom -= 0.02;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            cam.translate(-3, 0, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            cam.translate(3, 0, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            cam.translate(0, -3, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            cam.translate(0, 3, 0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            cam.rotate(-rotationSpeed, 0, 0, 1);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.E)) {
-            cam.rotate(rotationSpeed, 0, 0, 1);
-        }
+ if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+ cam.zoom += 0.02;
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+ cam.zoom -= 0.02;
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+ cam.translate(-3, 0, 0);
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+ cam.translate(3, 0, 0);
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+ cam.translate(0, -3, 0);
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+ cam.translate(0, 3, 0);
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+ cam.rotate(-rotationSpeed, 0, 0, 1);
+ }
+ if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+ cam.rotate(rotationSpeed, 0, 0, 1);
+ }
 
-        cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
+ cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
 
-        float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
-        float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+ float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+ float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
 
-        cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
-        cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
-    }
+ cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+ cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+ }
  */
+
+/*       Boolean fullScreen = Gdx.graphics.isFullscreen();
+        Graphics.DisplayMode currentMode = Gdx.graphics.getDisplayMode();
+        if (fullScreen == true)
+            Gdx.graphics.setWindowedMode(currentMode.width, currentMode.height);
+        else
+            Gdx.graphics.setFullscreenMode(currentMode);
+*/
