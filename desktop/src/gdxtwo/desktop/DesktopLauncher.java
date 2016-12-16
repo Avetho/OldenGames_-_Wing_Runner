1ed539d3a1b4e51fb87688c7513cc39e848dfb6a
matts6206 <matts6206@FHC-IC0034099.wrdsb.ca>
2016-12-16 13:08:34 -0500

    All the things. Launcher commit.

--- desktop/src/gdxtwo/desktop/DesktopLauncher.java
+++ desktop/src/gdxtwo/desktop/DesktopLauncher.java
@@ -1,15 +1,17 @@
 package gdxtwo.desktop;
 
+import GameV1.GameCore;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import GameV1.GameCore;
 
 public class DesktopLauncher {
 
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1320;
-        config.height = 860;
+        config.width = 1280;
+        config.height = 720;
+        //config.width = 1320;
+        //config.height = 860;
         config.fullscreen = false;
         config.foregroundFPS = 600;
         config.backgroundFPS = 600;
